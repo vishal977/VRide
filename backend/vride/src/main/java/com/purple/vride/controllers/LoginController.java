c4c4b2541ab8e53c9109a62c469f0e4dfcd5fa1f
vishal <vishalnani1999@gmail.com>
2020-02-13 06:39:36 +0530

    UPDATE Backend

--- backend/vride/src/main/java/com/purple/vride/controllers/LoginController.java
+++ backend/vride/src/main/java/com/purple/vride/controllers/LoginController.java
@@ -1,15 +1,54 @@
 package com.purple.vride.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.purple.vride.models.User;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.purple.vride.repositories.UserRepository;
 
 @RestController
 public class LoginController {
 	
-	@RequestMapping("/login")
-	public String logIn()
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@PostMapping("/login")
+	public String logIn(@RequestParam String id, @RequestParam String password)
 	{
-		return "SUCCESS";
+		List<User> users = (List<User>) userRepository.findAll();
+		StringBuilder response = new StringBuilder("");
+		try 
+		{
+			Iterator iter = users.iterator();
+			while(iter.hasNext())
+			{
+				User us = (User) iter.next();
+				if(id.equals(""+us.getId()))
+				{
+					if(us.getPassword().equals(password))
+					{
+						response.append("SUCCESS");
+					}
+					else
+					{
+						response.append("FAILURE");
+					}
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			return e.getMessage();
+		}
+		
+		return response.toString();
 	}
 
 }
